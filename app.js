d5eaa8750e579ebb6267aae67aae2e86fecc3ac1
Hede <Heatherj.swift@gmail.com>
2021-04-15 13:01:55 -0400

    Closes #2; generate the html file

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);  
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================');
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+  console.log('Portfolio complete! Check out index.html to see the output!')
+});
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//       console.log(profileDataArr[i]);  
+//     }
+
+//     console.log('================');
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
