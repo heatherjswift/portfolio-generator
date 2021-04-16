a032f1bbf41e4670938c5ca089a9aec769fa3ca3
Hede <Heatherj.swift@gmail.com>
2021-04-15 23:25:58 -0400

    create git ignore file

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
+const inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-  console.log('Portfolio complete! Check out index.html to see the output!')
-});
-
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);  
-//     }
-
-//     console.log('================');
-
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
 
